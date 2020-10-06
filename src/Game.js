f3e1a635849049bf87bf6a228978ebf39f80000d
VitaliyBiletov <vitaxa17@yandex.ru>
2020-10-06 16:25:29 +0300

    Добавление класса Game Исправление ошибок

---
+++ src/Game.js
@@ -0,0 +1,26 @@
+import {Drop} from "./Drop";
+
+export class Game {
+    start(){
+        const words = ['и','ш','й','л','у'];
+        const body = document.querySelector('body');
+        const clientWidth = document.documentElement.clientWidth;
+        const drop = new Drop(getRandomWord(words));
+        const randomPosX = Math.random() * clientWidth - drop.getWidth();
+        body.appendChild(drop.container);
+        drop.go(randomPosX);
+        setInterval(()=>{
+            const drop = new Drop(getRandomWord(words));
+            body.appendChild(drop.container);
+            const min = drop.getWidth();
+            const max = document.documentElement.clientWidth - min;
+            const randomPosX = Math.round(Math.random() * (max - min) + min);
+            drop.go(randomPosX);
+        }, 2000)
+    }
+}
+const getRandomWord = (words) => {
+    const randomNum = Math.round(Math.random() * (words.length - 1));
+    return words[randomNum];
+}
+
--- src/application.js
+++ src/application.js
@@ -3,74 +3,57 @@ import box2d from 'box2d';
 import { Drop } from './Drop.js';
 import dropImg from './img/drop-info-img.png'
 import CircleType from 'circletype';
+import {Game} from './Game';
 
 export default () => {
-    //const drop = document.querySelector('.drop');
-    const words = ['и','ш','й','л','у'];
-    //Создание картинки
-    const button = document.createElement('button');
-    $(button).addClass('button');
-    $(button).text('Начать');
-    $('.container-start-button').append(button);
 
-    //---------------------//
-    //Создание кнопки
-    const dropInfoImg = document.querySelector('.drop-info-img');
-    const dropTagImg = document.createElement('img');
-    dropTagImg.src = dropImg;
-    //console.log(dropInfoImg);
-    dropInfoImg.appendChild(dropTagImg);
-    //---------------------//
-    //Создание заголовка
-    const containerTitle = document.querySelector('.container-title');
-    //const title = document.createElement('h1');
-    containerTitle.textContent = 'Буквапельки';
-    //containerTitle.appendChild(title);
-    //---------------------//
 
     $(document).ready(()=>{
+        //Создание картинки
+        const button = document.createElement('button');
+        $(button).addClass('button');
+        $(button).text('Начать');
+        $('.container-start-button').append(button);
+        //---------------------//
+
+        //Создание кнопки
+        const dropInfoImg = document.querySelector('.drop-info-img');
+        const dropTagImg = document.createElement('img');
+        dropTagImg.src = dropImg;
+        //console.log(dropInfoImg);
+        dropInfoImg.appendChild(dropTagImg);
+        //---------------------//
+
+        //Создание заголовка
+        const containerTitle = document.querySelector('.container-title');
+        const title = document.createElement('h1');
+        containerTitle.textContent = 'Буквапельки';
+        containerTitle.appendChild(title);
+        //---------------------//
+
         $('.cloud').fadeIn(1000);
         $('.container-start-button').fadeIn(2000);
         //$('.container-title').fadeIn(3000);
 
+
         const circleTitleText = new CircleType(document.querySelector('.container-title'));
         circleTitleText.radius(1000).dir(1);
 
-        $('.container-title').animate({
-            top: `+=${30}vh`,
-            opacity: 1,
-        }, 1000)
-
         $('.drop-info').animate({
-            right: `-${5}vh`,
-        })
+            right: `-${3}vh`
+        }, { duration: 1000, queue: false})
 
-        //$('.drop-info').addClass('go');
+        $('.container-title').animate({
+            top: `+=${25}vh`,
+            opacity: 1,
+        }, { duration: 1000, queue: false})
+
         $('.button').click(function(){
-            $('.container-start-button').hide(300);
-            $('.container-title').hide(300);
-            $('.drop-info').hide(300);
-            // setTimeout(()=>{
-                const body = document.querySelector('body');
-                const clientWidth = document.documentElement.clientWidth;
-                const drop = new Drop(getRandomWord(words));
-                const randomPosX = Math.random() * clientWidth - drop.getWidth();
-                body.appendChild(drop.container);
-                drop.go(randomPosX);
-                setInterval(()=>{
-                    const drop = new Drop(getRandomWord(words));
-                    body.appendChild(drop.container);
-                    const min = drop.getWidth();
-                    const max = document.documentElement.clientWidth - min;
-                    console.log('min ', min);
-                    console.log('max ', max);
-                    const randomPosX = Math.round(Math.random() * (max - min) + min);
-                    drop.go(randomPosX);
-
-                }, 2000)
-            // }, 500)
-            console.log('width:', drop.getWidth())
-            console.log(document.documentElement.clientWidth)
+            $('.button').fadeOut(200);
+            $('.container-title').hide(400);
+            $('.drop-info').hide(200);
+            const game = new Game();
+            game.start();
         })
         })
 }
